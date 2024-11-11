e3ae717a462476880846b86ff7fcbdeb7e571938
Martin-Frei <mat.frei@gmx.de>
2024-11-11 15:37:10 +0100

    Add files via upload

--- Number_translator.py
+++ Number_translator.py
@@ -13,9 +13,9 @@ def number_to_words(number):
     
     # This dic are use to replace the actual digit to written number
     ones_dic ={'1' : 'one','2':'two', '3':'three', '4':'four', '5':'five', '6': 'six', '7': 'seven','8': 'eight', '9': 'nine', '0': ''}
-    tens_dic ={'1' : 'ten', '2':'twenthy', '3': 'thirty', '4' : 'fourty', '5':'fifty', '6': 'sixthy', '7': 'seventy', '8': 'eighty', '9':'ninety','0': ''}
+    tens_dic ={'1' : 'ten', '2':'twenty', '3': 'thirty', '4' : 'forty', '5':'fifty', '6': 'sixty', '7': 'seventy', '8': 'eighty', '9':'ninety','0': ''}
     place_value_dic ={0 : '', 1 :'', 2 :'hundred', 3 : 'thousand', 4 : '', 5 : '', 6 : 'million', 7 :'', 8 :'', 9 :'billion', 10 : '', 11: ''}
-    teens_dic ={'11' :'eleven', '12' : 'twelve', '13' : 'thirteen', '14' : 'fourteen', '15' : 'fifteen', '16' : 'sixteen', '17' : 'seventeen', '18' : 'eighteen', '19' : 'nineteen'}
+    teens_dic ={'10' : 'ten', '11' :'eleven', '12' : 'twelve', '13' : 'thirteen', '14' : 'fourteen', '15' : 'fifteen', '16' : 'sixteen', '17' : 'seventeen', '18' : 'eighteen', '19' : 'nineteen'}
     
     translation = ''
     
@@ -51,22 +51,26 @@ def number_to_words(number):
     # Join the list to a string with only 1 space between
     translation = ' '.join(list)
     return translation
-    
-    
-# Usage example
-while True :
-    user_number = input('Give me a number with max 12 digit:  ')
-    if len(user_number) > 12:
-        print('Sorry, to many digit. Try again')
-        continue
-    if not user_number.isdigit():
-        print('Sorry, wrong input. Try again')
-        continue
-    if user_number == '0':
-        print('zero')
-    else :        
-        print(number_to_words(int(user_number)))
-        break
+ 
+# print(__name__)
+ 
+if __name__ == '__main__' :
+       
+    # print('I come from file 1 ')    
+    # Usage example
+    while True :
+        user_number = input('Give me a number with max 12 digit:  ')
+        if len(user_number) > 12:
+            print('Sorry, to many digit. Try again')
+            continue
+        if not user_number.isdigit():
+            print('Sorry, wrong input. Try again')
+            continue
+        if user_number == '0':
+            print('zero')
+        else :        
+            print(number_to_words(int(user_number)))
+            break
 
 
 
